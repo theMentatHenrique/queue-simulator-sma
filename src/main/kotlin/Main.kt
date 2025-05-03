1dfca2a4aab91c1cbfe6fe663f19b9e02100b6fb
Henrique Feijó <121191246+henrikFeijo@users.noreply.github.com>
2025-05-02 22:56:50 -0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,6 @@ import br.com.pucrs.factory.RandomNumbersManager
 
 fun main() {
     var factory = QueueFactory()
-    val p = 2999413E9
     val queues = factory.readQueuesAndNetworksFromYaml("/Users/henriquefeijopaim/Documents/Pessoal/queue-simulator-sma/src/main/kotlin/factory/model-aux.yml")
     var numbers = RandomNumbersManager.generateNumbersByLimit(limit = factory.getrandom(), factory.getArrivalTime().toInt())
 
